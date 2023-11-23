4a2e25475a4411c86ccfb58f530a66da6ebddd35
Devyanshu0911 <77067151+Devyanshu0911@users.noreply.github.com>
2023-11-23 15:11:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_map(loc):
 def run():
     st.title("Landmark Recognition")
     img = PIL.Image.open('logo.png')
-    img = img.resize(720,196.27)
+    img = img.resize((720,196))
     st.image(img)
     img_file = st.file_uploader("Choose your Image", type=['png', 'jpg'])
     if img_file is not None:
